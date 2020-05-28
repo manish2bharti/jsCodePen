4b710de615ea49d8b335cf93b79ea8899b23f7b3
Manish Bharti <manish.bharti@kagamierp.com>
2020-05-29 02:36:42 +0530

    add  external resources css/js

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,34 @@ var compile = () => {
       "</script>"
   );
   var head = code.getElementsByTagName('head')[0];
-  var script = code.createElement('script');
-  script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
-  script.type = 'text/javascript';
-  head.appendChild(script);
+  // Inject external CSS files in iframe
+  if(addedExternalResources.cssResources.length){
+    var resources = addedExternalResources.cssResources;
+    resources.forEach(function(item, index){
+      var link = code.createElement('link');
+      link.href = item; //https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+      link.type = 'text/css';
+      link.rel = "stylesheet";
+      head.appendChild(link); 
+    })
+  }
+
+  // Inject external JS files in iframe
+  if(addedExternalResources.jsResources.length){
+    var resources = addedExternalResources.jsResources;
+    resources.forEach(function(item, index){
+      var script = code.createElement('script');
+      script.src = item; //https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+      script.type = 'text/javascript';
+      head.appendChild(script); 
+    })
+  }
+ 
   code.close();
   [].forEach.call(code.querySelectorAll("script"), function (el, idx) {
+    document.getElementById("code").contentWindow.eval(el);
+  });
+  [].forEach.call(code.querySelectorAll("link"), function (el, idx) {
     document.getElementById("code").contentWindow.eval(el);
   });
 }
@@ -57,3 +79,38 @@ var openViewSettings = (evt, view) => {
   // evt.currentTarget.className += " active";
 }
 // --------------- Tabs Functionality End ---------------------
+
+
+// --------------- Add resources Functionality Start ---------------------
+var i = 1;
+
+function addResource(containerId, type) {
+    i++;
+    var div = document.createElement('div');
+    var id=++document.querySelectorAll('#jsResources input[type=text]')[document.querySelectorAll('#jsResources input[type=text]').length-1].name.split("_")[1];
+
+    div.innerHTML = `Resource ${id} : <input type="text" name="${type}_${id}"/>
+    <input type="button" id="add_${type}()_${id}" onclick="addResource('${containerId}','${type}')" value="+" />
+    <input type="button" id="rem_${type}()_${id}" onclick="removeResource(this, '${containerId}')" value="-" />`;
+
+    document.getElementById(containerId).appendChild(div);
+}
+
+function removeResource(div, containerId) {
+  document.getElementById(containerId).removeChild(div.parentNode);
+  i--;
+}
+
+var addedExternalResources = {
+  'jsResources': [],
+  'cssResources': []
+}
+function captureResponse(type){
+  addedExternalResources[type] = [];
+  for(var j=0;j<document.querySelectorAll('#'+type+' input[type=text]').length;j++){
+    addedExternalResources[type].push(document.querySelectorAll('#'+type+' input[type=text]')[j].value);
+  }
+  addedExternalResources[type] = addedExternalResources[type].filter(function(el) { return el; });
+}
+
+// --------------- Add resources Functionality End ---------------------
